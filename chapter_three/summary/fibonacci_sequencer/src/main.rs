76a1d16ac9743530a08af59520ab35c9df4b748e
“Austin <austinkloske@gmail.com>
2022-06-20 13:17:22 +0200

    Fibonacci sequencer

--- chapter_three/summary/fibonacci_sequencer/src/main.rs
+++ chapter_three/summary/fibonacci_sequencer/src/main.rs
@@ -8,13 +8,30 @@ fn main() {
         .read_line(&mut integer_input)
         .expect("Failed to read line");
         
-    // let guess: u32 = guess.trim().parse().expect("Please type a number!");
-    let guess: u32 = match integer_input.trim().parse() {
+    let integer: u32 = match integer_input.trim().parse() {
         Ok(num) => num,
         Err(_) => { 
             panic!( "That wasn't valid input! Index must be a positive integer" )
         },
     };
 
-    println!("You entered: {}", integer_input);
+    println!("You entered: {}", integer);
+    iterate(integer);
+}
+
+fn fib (n: u32) -> u32 {
+    if n <= 0 {
+          return 0;
+    } else if n== 1{
+          return 1;
+} else {
+    return fib (n-1)  + fib(n-2);
+ }
+}
+
+fn iterate(x: u32) {
+    // let mut fibonacci_values: (i32, i32) = (0, 1);
+    for index in 0..x {
+        println! ( "fibonacci ({}) => {}", index, fib(index));
+    }
 }
