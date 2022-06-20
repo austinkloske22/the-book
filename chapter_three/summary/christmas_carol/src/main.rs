fe6ba511a9ab15b4bf4b49e46815ad7299214b2b
“Austin <austinkloske@gmail.com>
2022-06-20 13:49:23 +0200

    a christmas carol

--- chapter_three/summary/christmas_carol/src/main.rs
+++ chapter_three/summary/christmas_carol/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    let days = ["first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth"];
+    
+    let gifts = ["a partridge in a pear tree", "Two turtledoves", "Three French hens", "Four calling birds", "Five gold rings", "Six geese a-laying",
+              "Seven swans a-swimming", "Eight maids a-milking", "Nine ladies dancing", "Ten lords a-leaping", "Eleven pipers piping", "Twelve drummers drumming", ];
+    
+    for day in 0..12 {
+        println!("\nOn the {} day of Christmas", days[day]);
+        println!("My true love sent to me");
+        
+        for gift in (0..day + 1).rev() {
+            if day > 0 && gift == 0 {
+                print!("And ");
+            }
+            println!("{}", gifts[gift]);
+        }
+    }
 }
